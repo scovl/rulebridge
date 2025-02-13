94efa77c4b98289f8046e62401516ff66cc548b0
Vitor Lobo Ramos <lobocode@gmail.com>
2025-02-13 12:55:49 -0300

    refactor: migrate config to proper package structure

--- config.py
+++
@@ -1,7 +0,0 @@
-# Credenciais de autenticação
-CLIENT_ID = ""
-CLIENT_KEY = ""
-REALM = ""
-
-# Configuração de proxies (se necessário)
-PROXIES = {} 
---
+++ src/config/settings.py
@@ -0,0 +1,7 @@
+# Authentication credentials for Stackspot API
+CLIENT_ID = ""
+CLIENT_KEY = ""
+REALM = ""
+
+# Proxy settings (if required)
+PROXIES = {} 
