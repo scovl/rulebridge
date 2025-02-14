3f11e25d76147126da5e2b9044586ff8f9cadeda
Vitor Lobo Ramos <lobocode@gmail.com>
2025-02-14 08:20:28 -0300

    refactor: update XML templates with metadata

--- src/core/templates.py
+++ src/core/templates.py
@@ -1,11 +1,11 @@
+from .constants import PMD_RULE_METADATA, SEVERITY_MAPPING
+
 class XMLTemplates:
-    XML_HEADER = """<?xml version="1.0"?>
-<ruleset name="Custom Rules"
-    xmlns="http://pmd.sourceforge.net/ruleset/2.0.0"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://pmd.sourceforge.net/ruleset/2.0.0 https://pmd.sourceforge.io/ruleset_2_0_0.xsd">
-    
-    <description>Custom rule generated via AI</description>"""
+    XML_HEADER = f"""<?xml version="1.0" encoding="UTF-8"?>
+<ruleset name="{PMD_RULE_METADATA['RULESET_NAME']}"
+         xmlns="{PMD_RULE_METADATA['RULESET_XMLNS']}"
+         xmlns:xsi="{PMD_RULE_METADATA['RULESET_XSI']}"
+         xsi:schemaLocation="{PMD_RULE_METADATA['RULESET_SCHEMA_LOCATION']}">"""
 
     XML_FOOTER = """
 </ruleset>"""
@@ -14,27 +14,14 @@ class XMLTemplates:
     <rule name="{name}"
           language="{language}"
           message="{message}"
-          class="net.sourceforge.pmd.lang.rule.XPathRule"
-          externalInfoUrl="https://pmd.github.io/latest/pmd_rules_java.html">
-          
-        <description>{description}</description>
+          class="{rule_class}">
+        <description>{severity_tag}</description>
         <priority>{severity}</priority>
         <properties>
             <property name="xpath">
                 <value>
-                <![CDATA[
                 {xpath}
-                ]]>
                 </value>
             </property>
         </properties>
-        <example>
-            <![CDATA[
-            // Good code example
-            {good_example}
-            
-            // Bad code example
-            {bad_example}
-            ]]>
-        </example>
     </rule>""" 
